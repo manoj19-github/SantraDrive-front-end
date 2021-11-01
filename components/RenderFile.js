007add6fc71e4d030672770859a064e1075eb217
Manoj Santra <santramanoj1997@gmail.com>
2021-11-02 04:55:49 +0530

    first commit

---
+++ components/RenderFile.js
@@ -0,0 +1,18 @@
+import {FunctionComponent} from "react"
+import Image from "next/image"
+import {sizeInMb} from "libs/sizeInMb"
+const RenderFile=({file})=>{
+
+  return(
+    <div className="d-flex justify-content-center align-items-center " style={{width:"100%",margin:"1rem 0"}}>
+      <Image src={`/fileTypes/${file.format}.png`} alt="" width="70" height="70"/>
+      <div className="d-flex  justify-content-around align-items-center" style={{width:"75%"}}>
+        <div>{file.name}</div>
+        <div>{sizeInMb(file.sizeInBytes)}</div>
+
+      </div>
+
+    </div>
+  )
+}
+export default RenderFile
---
+++ libs/sizeInMb.js
@@ -0,0 +1,3 @@
+export const sizeInMb=(bytes)=>(
+  `${(bytes/(1024*1024)).toFixed(2)} MB`
+)
